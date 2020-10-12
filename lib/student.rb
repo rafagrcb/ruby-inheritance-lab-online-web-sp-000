730331e2d149c9d0e7977a0afa6421f54996eaf1
Rafael Garcia <rafagrcb@gmail.com>
2020-10-12 18:42:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
 class Student 
+  
+  attr_accessor :knowledge
+  
+  class Student < User
+  
+  def initialize
+    @knowledge = []
+  end
 
+  def learn(string)
+    knowledge << string
+  end
 end
